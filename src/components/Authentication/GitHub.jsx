bfd01da318894d01a365bd4679d0f6997bbb2389
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2022-06-06 11:38:22 +0000

    GitHub Intgeration. [ WIP ]

--- src/components/Authentication/GitHub.jsx
+++ src/components/Authentication/GitHub.jsx
@@ -1,7 +1,9 @@
+// https://levelup.gitconnected.com/how-to-implement-login-with-github-in-a-react-app-bd3d704c64fc
+
 import LoginGithub from "react-login-github";
 import React from "react";
 
-const onSuccess = response => console.log(response);
+const onSuccess = response => console.log(response.code);
 const onFailure = response => console.error(response);
 
 let client_id = process.env.REACT_APP_CLIENT_ID
