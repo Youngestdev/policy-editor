b261a0d2102bddc6af38902fed01602a45f5ce9b
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2022-06-11 22:22:16 +0000

    Minor work on the component. WIP.

--- src/components/Policies/UpdatePolicy.jsx
+++ src/components/Policies/UpdatePolicy.jsx
@@ -1,9 +1,13 @@
+import { FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import request from "../../utils/request";
 
 const UpdatePolicy = () => {
-    const [policy, setPolicy] = useState({});
+    const [policy, setPolicy] = useState({
+        name: "",
+        rules: [],
+    });
     let params = useParams()
 
     useEffect(() => {
@@ -11,13 +15,22 @@ const UpdatePolicy = () => {
         // and set the state to the policies
         request.get(`policies/${params.name}`)
         .then((response) => {
-            console.log(response.data);
-            setPolicy(response.data);
+            setPolicy({
+                name: response.data.name,
+                rules: [...response.data.rules],
+            });
+            console.log(policy)
         }).catch((error) => {
             console.log(error);
         }
         )
     }, [])
+
+    return (
+        <>
+            <Heading as="h2">Update Policy - { policy.name } </Heading>
+        </>
+    )
 }
 
 export default UpdatePolicy;
