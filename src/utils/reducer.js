8ef363bae7993a3c1097dc63780f5e2f4ef026f4
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2022-06-20 12:51:12 +0000

    Add the time of log in to local storage

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,6 +2,7 @@ export const initialState = {
     // user: JSON.parse(localStorage.getItem('user')) || null,
     access_token: localStorage.getItem('access_token') || null,
     isLoggedIn: JSON.parse(localStorage.getItem('isLoggedIn')) || false,
+    loggedInTime: localStorage.getItem('loggedInTime') || null
 }
 
 export const reducer = (state, action) => {
@@ -10,12 +11,14 @@ export const reducer = (state, action) => {
             // set the user and access_token to the localstorage
             localStorage.setItem('user', JSON.stringify(action.payload.user));
             localStorage.setItem('access_token', action.payload.access_token);
-            localStorage.setItem('isLoggedIn', true);          
+            localStorage.setItem('isLoggedIn', true);
+            localStorage.setItem('loggedInTime', new Date());        
         return {
             ...state,
             user: action.payload.user,
             access_token: action.payload.access_token,
             isLoggedIn: true,
+            loggedInTime: new Date()
         };
         case 'LOGOUT':
             // clear the localstorage
