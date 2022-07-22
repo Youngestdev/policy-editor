ebc82f76bbe3cd5e011fd1e188a4d27154fa5ea0
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-07-22 10:19:16 +0100

    Update the GitLab authentication.

--- src/components/Authentication/GitLab.jsx
+++ src/components/Authentication/GitLab.jsx
@@ -1,4 +1,4 @@
-import { Button, Link } from "@chakra-ui/react";
+import { Center, Spinner, Link } from "@chakra-ui/react";
 import axios from "axios";
 import { useContext } from "react";
 import { Navigate } from "react-router";
@@ -8,11 +8,11 @@ import { AuthContext } from "../../index";
 
 const GitLabAuth = () => {
 
-  const GITLAB_URL = `https://gitlab.com/oauth/authorize?client_id=${process.env.REACT_APP_GITLAB_APP_ID}&redirect_uri=${process.env.REACT_APP_GITLAB_REDIRECT_URI}&response_type=code&scope=read_user%20read_repository`;
+  const GITLAB_URL = `https://gitlab.com/oauth/authorize?client_id=${process.env.REACT_APP_GITLAB_APP_ID}&redirect_uri=${process.env.REACT_APP_GITLAB_REDIRECT_URI}&response_type=code&scope=profile%20api%20read_api%20write_repository%20email`;
   const { state, dispatch } = useContext(AuthContext); 
 
   if (state.isLoggedIn) {
-    return <Navigate to="/" />
+    return <Navigate to="/policies" />
   }
 
   return (
@@ -23,43 +23,57 @@ const GitLabAuth = () => {
 }
 
 export const HandleAccessToken = () => {
-  const { state, dispatch } = useContext(AuthContext); 
-
+  const { state, dispatch } = useContext(AuthContext);
+  
+  // Check if the url has the GitLab code with it.
   const location = useLocation();
   const code = location.search.substring(6);
+
+  if (state.isLoggedIn) {
+    return <Navigate to="/policies" />
+  }
 
   if (code.length < 64 ) {
     if (state.isLoggedIn) {
       return <Navigate to="/policies" />
     }
-    return <Navigate to="/" />;
+    return <Navigate to="/login" />;
   }
 
   // make a request to the gitlab api to get the access token
   axios
-    .get(
+    .post(
       // Temporary address until I'm able to fix the direct interaction with GitLab
-      `https://web-youngestdev.cloud.okteto.net/api/auth/callback/gitlab?code=${code}&redirect_uri=${process.env.REACT_APP_GITLAB_REDIRECT_URI}&client_id=${process.env.REACT_APP_GITLAB_APP_ID}&client_secret=${process.env.REACT_APP_GITLAB_SECRET}`
-    )
+      `https://gitlab.com/oauth/token`, {
+          code: code,
+          client_id: process.env.REACT_APP_GITLAB_APP_ID,
+          client_secret: process.env.REACT_APP_GITLAB_SECRET,
+          grant_type: "authorization_code",
+          redirect_uri: process.env.REACT_APP_GITLAB_REDIRECT_URI
+        }
+  )
     .then((response) => {
-      console.log("Response from GitLab: ", response)
       // store the access token in the state
       dispatch({
         type: "LOGIN",
         payload: response.data,
+        provider: "gitlab",
       });
     }
     )
     .catch((error) => {
       console.log(error);
     }
-    );    
+    );
 
     if (state.isLoggedIn) {
       return <Navigate to="/policies" />
     }
+  
   return (
-    <p>This is the first test here.</p>
+     <Center>  
+      <Spinner size='xl' /> 
+     </Center>
   )
 } 
 
